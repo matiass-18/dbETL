446c3909d6d83d0a93077f4a6cb372d15cf02a72
Matias Chahin Toledo <matiaschahintoledo18@gmail.com>
2025-02-27 02:40:45 -0500

    Add files via upload

---
+++ removeSpaces.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 02:36:39 2025
+
+@author: Matias
+"""
+
+import json
+
+ruta_json = "C:/Users/Matias/Desktop/Uni/trabajosU/BigData/nuevoETL/dbJson.json"
+
+with open(ruta_json, "r", encoding="utf-8") as f:
+    datos = json.load(f)
+
+def limpiar_clave(clave):
+    return clave.lower().replace(" ", "_").replace("/", "_").replace("\\", "_")
+
+datos_modificados = [{limpiar_clave(k): v for k, v in doc.items()} for doc in datos]
+
+ruta_salida = "C:/Users/Matias/Desktop/Uni/trabajosU/BigData/nuevoETL/dbJson_modificado.json"
+with open(ruta_salida, "w", encoding="utf-8") as f:
+    json.dump(datos_modificados, f, indent=4, ensure_ascii=False)
+
+print(f"Archivo modificado guardado en: {ruta_salida}")
